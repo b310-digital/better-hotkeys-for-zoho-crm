5e7c544406b7f535f06be20d1d072ed9cb83e7c4
Jannik Streek <jannik.streek@gmail.com>
2022-11-09 17:21:52 +0100

    add react view as start

--- esbuild.js
+++ esbuild.js
@@ -1,14 +1,21 @@
-require('esbuild').build({
-    entryPoints: [
-        "./src/background.ts"
-    ],
+require("esbuild")
+  .build({
+    entryPoints: ["./src/background.ts", "./src/popup.tsx"],
     bundle: true,
     minify: true,
     sourcemap: process.env.NODE_ENV !== "production",
     target: ["chrome89", "firefox91"],
-    outdir: './public/build',
+    outdir: "./public/build",
     define: {
-        "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`
+      "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
     },
-    loader: { '.eot': 'file', '.woff': 'file', '.woff2': 'file', '.ttf': 'file', '.svg': 'file', '.html': 'file' }
-}).catch(() => process.exit(1))
+    loader: {
+      ".eot": "file",
+      ".woff": "file",
+      ".woff2": "file",
+      ".ttf": "file",
+      ".svg": "file",
+      ".html": "file",
+    },
+  })
+  .catch(() => process.exit(1));
