6099e86dc18479939ad993bad19ec9fb3a77b1ef
JannikStreek <Jannik.streek@gmail.com>
2022-11-16 17:43:41 +0100

    add zoho field that extracts fields from simple link copy paste (#18)
    * add zoho field that extracts fields from simple link copy paste
    
    * refactor: Add type definitions to avoid assignment errors
    
    * feat: Added other Zoho CRM regions
    
    Co-authored-by: Gerardo Navarro <gerardo@b310.de>

--- src/Popup/App.tsx
+++ src/Popup/App.tsx
@@ -1,9 +1,12 @@
 import { string } from "prop-types";
-import React, { useEffect, useState } from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 
 export default function App() {
-  const [selectedServerLocation, setServerLocation] = useState();
-  const [zohoOrgId, setZohoOrgId] = useState();
+  const [selectedServerLocation, setServerLocation] = useState<
+    string | undefined
+  >();
+  const [zohoOrgId, setZohoOrgId] = useState<string | undefined>();
+  const [zohoLink, setZohoLink] = useState<string | undefined>();
 
   useEffect(() => {
     chrome.storage.local.get("location", (result) => {
@@ -35,10 +38,44 @@ export default function App() {
     saveData("zohoId", event.target.value);
   };
 
+  const handleZohoUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === "") return;
+
+    const regexp = new RegExp(
+      /crm.zoho.(?<location>eu|com|com.au|com.cn|in|jp)\/crm\/(?<zohoId>org\d*)\/tab\//
+    );
+    const matches = regexp.exec(event.target.value);
+
+    if (matches?.groups === undefined) return;
+
+    const zohoId: string = matches?.groups?.zohoId || "";
+    const zohoLocation: string = matches?.groups?.location || "";
+    setZohoOrgId(zohoId);
+    setServerLocation(zohoLocation);
+    setZohoLink(event.target.value);
+    saveData("zohoId", zohoId);
+    saveData("location", zohoLocation);
+  };
+
   return (
     <div className="columns is-centered">
       <div className="column">
         <h1 className="title">Zoho Settings</h1>
+        <div className="field">
+          <label className="label">Zoho Link</label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              placeholder="https://..."
+              onChange={handleZohoUrlChange}
+            />
+          </div>
+          <p className="help">
+            Paste the zoho link to retrieve the needed information
+          </p>
+        </div>
+        <hr />
         <div className="field">
           <label className="label">Org ID</label>
           <div className="control">
@@ -63,8 +100,12 @@ export default function App() {
                 value={selectedServerLocation}
               >
                 <option></option>
-                <option>eu</option>
+                <option>com.au</option>
+                <option>com.cn</option>
                 <option>com</option>
+                <option>eu</option>
+                <option>in</option>
+                <option>jp</option>
               </select>
             </div>
           </div>
