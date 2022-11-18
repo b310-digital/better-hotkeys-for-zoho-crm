65c5f9ae8171f4f40b1132d13e8ec714ab747ce7
Jannik Streek <jannik.streek@gmail.com>
2022-11-18 16:37:14 +0100

    started styling

--- src/Popup/App.tsx
+++ src/Popup/App.tsx
@@ -1,5 +1,4 @@
-import { string } from "prop-types";
-import React, { SetStateAction, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
   const [selectedServerLocation, setServerLocation] = useState<
@@ -10,7 +9,8 @@ export default function App() {
 
   useEffect(() => {
     chrome.storage.local.get("location", (result) => {
-      setServerLocation(result.location);
+      const location = result.location || 'com'
+      setServerLocation(location);
     });
 
     chrome.storage.local.get("zohoId", (result) => {
@@ -58,57 +58,96 @@ export default function App() {
   };
 
   return (
-    <div className="columns is-centered">
-      <div className="column">
-        <h1 className="title">Zoho Settings</h1>
-        <div className="field">
-          <label className="label">Zoho Link</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              placeholder="https://..."
-              onChange={handleZohoUrlChange}
-            />
+    <div>
+      <div className="columns is-centered">
+        <div className="column">
+          <img src="../../public/images/zoho_better_hotkeys_icon.png" />
+          <h1 className="title">Zoho Settings</h1>
+          <div className="field">
+            <label className="label">Zoho Link</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="https://..."
+                onChange={handleZohoUrlChange}
+              />
+            </div>
+            <p className="help">
+              Paste the zoho link to retrieve the needed information
+            </p>
           </div>
-          <p className="help">
-            Paste the zoho link to retrieve the needed information
-          </p>
-        </div>
-        <hr />
-        <div className="field">
-          <label className="label">Org ID</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              placeholder="org..."
-              onChange={handleZohoOrgIdChange}
-              value={zohoOrgId}
-            />
+          <hr />
+          <div className="field">
+            <label className="label">Org ID</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="org..."
+                onChange={handleZohoOrgIdChange}
+                value={zohoOrgId}
+              />
+            </div>
+            <p className="help">
+              You can find this information in the URL (org...)
+            </p>
           </div>
-          <p className="help">
-            You can find this information in the URL (org...)
-          </p>
-        </div>
-        <div className="field">
-          <label className="label">Server Location</label>
-          <div className="control">
-            <div className="select">
-              <select
-                onChange={handleServerLocationChange}
-                value={selectedServerLocation}
-              >
-                <option></option>
-                <option>com.au</option>
-                <option>com.cn</option>
-                <option>com</option>
-                <option>eu</option>
-                <option>in</option>
-                <option>jp</option>
-              </select>
+          <div className="field">
+            <label className="label">Server Location</label>
+            <div className="control">
+              <div className="select">
+                <select
+                  onChange={handleServerLocationChange}
+                  value={selectedServerLocation}
+                >
+                  <option></option>
+                  <option>com.au</option>
+                  <option>com.cn</option>
+                  <option>com</option>
+                  <option>eu</option>
+                  <option>in</option>
+                  <option>jp</option>
+                </select>
+              </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="columns is-centered">
+        <div className="column">
+          <h1 className="title">Available Hotkeys</h1>
+          <table className="table is-bordered is-striped is-hoverable">
+            <thead>
+              <tr>
+                <th>Function</th>
+                <th>Hotkey Windows</th>
+                <th>Hotkey Mac</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Open Accounts Module (global)</td>
+                <td><kbd>Alt</kbd> <kbd>Shift</kbd> <kbd>A</kbd></td>
+                <td><kbd>Option</kbd> <kbd>Shift</kbd> <kbd>A</kbd></td>
+              </tr>
+              <tr>
+                <td>Open Deals Module (global)</td>
+                <td><kbd>Alt</kbd> <kbd>Shift</kbd> <kbd>D</kbd></td>
+                <td><kbd>Option</kbd> <kbd>Shift</kbd> <kbd>D</kbd></td>
+              </tr>
+              <tr>
+                <td>Open Contacts Module (global)</td>
+                <td><kbd>Alt</kbd> <kbd>Shift</kbd> <kbd>C</kbd></td>
+                <td><kbd>Option</kbd> <kbd>Shift</kbd> <kbd>C</kbd></td>
+              </tr>
+              <tr>
+                <td>Open Leads Module (global)</td>
+                <td><kbd>Alt</kbd> <kbd>Shift</kbd> <kbd>L</kbd></td>
+                <td><kbd>Option</kbd> <kbd>Shift</kbd> <kbd>L</kbd></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
